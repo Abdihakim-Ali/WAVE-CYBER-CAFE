99e1b3dfbf9612192cc74bae086043df146d9aed
Abdihakim Ali <aliabdihakimahmed@gmail.com>
2026-09-02 19:23:13 +0300

    feat: optimize hero for large desktop displays

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ async function loadSection(id, path) {
     }
 }
 
-loadSection("navbar", "/src/sections/navbar/navbar.html");
-loadSection("hero", "/src/sections/hero/hero.html");
+loadSection("navbar", "./src/sections/navbar/navbar.html");
+loadSection("hero", "./src/sections/hero/hero.html");
 
 console.log("🌊 Wave Technology Hub");
