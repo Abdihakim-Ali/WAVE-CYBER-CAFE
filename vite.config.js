1e7fd8c32700a1a16da6602c47111bcbc33354c7
Abdihakim Ali <aliabdihakimahmed@gmail.com>
2026-09-20 11:18:04 +0300

    Fix Vite base path for local and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+// import { defineConfig } from "vite";
+
+// export default defineConfig(({ command }) => ({
+//     base: command === "build" ? "/WAVE-CYBER-CAFE/" : "/"
+// }));
+
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => ({
-    base: command === "build" ? "/WAVE-CYBER-CAFE/" : "/"
-}));
+export default defineConfig({
+    base: process.env.VITE_BASE || "/"
+});
